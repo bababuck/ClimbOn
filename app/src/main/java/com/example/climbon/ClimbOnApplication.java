21e228767e85bc85c3248e801ec0ea601bda9dbc
bababuck <buchner.ryan@gmail.com>
2021-12-19 12:38:34 -0800

    Extend application class to carry universal data.
    Probably unnecessary to have two classes like this.
    
    Maybe refactor?

---
+++ app/src/main/java/com/example/climbon/ClimbOnApplication.java
@@ -0,0 +1,12 @@
+package com.example.climbon;
+
+import android.app.Application;
+
+public class ClimbOnApplication extends Application {
+    /* Holds gloabl data for storage.
+
+    TODO:
+    Refator with Unviversal Data?
+    */
+    public UniversalData data = new UniversalData();
+}
