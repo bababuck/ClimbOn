f989bf78b7e260448d7d93a4a594213c918bb984
bababuck <buchner.ryan@gmail.com>
2021-12-30 00:55:15 -0800

    Finish up class (for now).
    Now should be able to create image button for the horizontal scroll.

--- app/src/main/java/com/example/climbon/RouteViewPanel.java
+++ app/src/main/java/com/example/climbon/RouteViewPanel.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class RouteViewPanel extends BoundaryView {
+public class RouteViewPanel extends androidx.appcompat.widget.AppCompatImageButton {
     /* Displays a panel with only the activated holds.
 
     To be used when viewing the entire route in route view.
@@ -19,6 +22,8 @@ public class RouteViewPanel extends BoundaryView {
     functionality in the panel view, but not sure if I could reuse here.
     */
     Boundary boundary;
+    ArrayList<Drawable> holds = new ArrayList<>();
+    ArrayList<Rect> bounds = new ArrayList<>();
     public RouteViewPanel(Context context) {
         super(context);
     }
@@ -27,22 +32,28 @@ public class RouteViewPanel extends BoundaryView {
         super(context, attributeSet);
     }
 
-    public RouteViewPanel(Context context, Boundary boundary, ArrayList<Integer> holds, ArrayList<Boolean> hold_status){
-        super(context, boundary);
-        holds[i] = context.getResources().getDrawable(R.drawable.my_image);
+    public RouteViewPanel(Context context, Boundary boundary, ArrayList<Coordinate> coords, ArrayList<Integer> holds, ArrayList<Boolean> hold_status){
+        super(context);
+        this.boundary = boundary;
+        for (int i=0;i<holds.size();++i){
+            if (hold_status.get(i)){
+                this.holds.add(context.getResources().getDrawable(PanelViewHold.getImageId(holds.get(i))));
+                bounds.add(new Rect(0,0,5,5));
+            }
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        /* Draws the shape and holds to canvas.
+        /* Draws the boundary and holds to canvas.
 
+        Everything needed is initialized in the constructor.
         */
         super.onDraw(canvas);
-
+        boundary.draw(canvas);
         for (int i=0; i<holds.size(); ++i){
-            if (hold_status[i]){
-                holds[i].setBounds(imageBounds);
-                holds[i].draw(canvas);
-            }
+            holds.get(i).setBounds(bounds.get(i));
+            holds.get(i).draw(canvas);
         }
     }
+}
