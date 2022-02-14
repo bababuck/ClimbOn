c3aa9f4a5d0f8a7401a8c499d1b8c5e55542b51d
bababuck <buchner.ryan@gmail.com>
2022-02-13 23:02:26 -0800

    Extend Routeview to path to set holds for SetHolds Activity.

---
+++ app/src/main/java/com/example/climbon/SetHoldsOuter.java
@@ -0,0 +1,21 @@
+package com.example.climbon;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SetHoldsOuter extends RouteView {
+
+    protected void setNavigation(ImageButton button, int i) {
+        /* Set the holds on the selected activity. */
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saved_data.current_shape = i;
+                Intent intent = new Intent(view.getContext(), SetHoldsActivity.class);
+
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+}
