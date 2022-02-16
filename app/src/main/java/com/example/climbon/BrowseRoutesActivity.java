cb66a5d06bc15be20dae94d29841aa7df002fc6a
bababuck <buchner.ryan@gmail.com>
2022-02-16 00:54:34 -0800

    Browse Routes Changes.

--- app/src/main/java/com/example/climbon/BrowseRoutesActivity.java
+++ app/src/main/java/com/example/climbon/BrowseRoutesActivity.java
@@ -2,7 +2,12 @@ package com.example.climbon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class BrowseRoutesActivity extends AppCompatActivity {
 
@@ -10,6 +15,8 @@ public class BrowseRoutesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_routes);
+        LinearLayout scroll = findViewById(R.id.ScrollLL);
+        LinearLayout.LayoutParams button_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
         UniversalData saved_data;
         ClimbOnApplication app = (ClimbOnApplication) getApplication();
@@ -17,6 +24,20 @@ public class BrowseRoutesActivity extends AppCompatActivity {
         // Create button for each route
         for (RouteData route : saved_data.route_data) {
             String name = route.name;
+
+            Button current_button = new Button(this);
+            current_button.setBackgroundColor(Color.GREEN);
+            current_button.setText("Confirm Panel");
+            current_button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    saved_data.current_route = route;
+                    Intent intent = new Intent(view.getContext(), RouteView.class);
+
+                    view.getContext().startActivity(intent);
+                }
+            });
+            current_button.setTextColor(Color.BLACK);
+            scroll.addView(current_button, button_params);
         }
     }
 
