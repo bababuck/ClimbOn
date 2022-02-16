b0458c714dca55a0e05a1b77094dd20de051ae97
bababuck <buchner.ryan@gmail.com>
2022-02-16 00:55:34 -0800

    Change int to string for Routdata.type

--- app/src/main/java/com/example/climbon/RouteData.java
+++ app/src/main/java/com/example/climbon/RouteData.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 class RouteData {
     int v_rating;
     ArrayList<Boolean> holds;
-    int type;
+    String type;
     String name;
-    public RouteData(int v_rating, ArrayList<Boolean> holds, int type, String name) {
+    public RouteData(int v_rating, ArrayList<Boolean> holds, String type, String name) {
         this.holds = holds;
         this.v_rating = v_rating;
         this.type = type;
