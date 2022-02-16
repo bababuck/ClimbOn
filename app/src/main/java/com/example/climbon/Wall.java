cb99df4898d697abb8f8d00b28aaccc46504ed39
bababuck <buchner.ryan@gmail.com>
2022-02-16 00:55:09 -0800

    Number of hold methods in wall.

--- app/src/main/java/com/example/climbon/Wall.java
+++ app/src/main/java/com/example/climbon/Wall.java
@@ -12,5 +12,30 @@ public class Wall {
 
     public ArrayList<Shape> panel_set = new ArrayList<>();
 
+    public ArrayList<Integer> findCumulativeHoldNumbers() {
+        /* Find the running total of number of holds.
+
+        Will allow us to locate the holds for a certain panel.
+        */
+        ArrayList<Integer> cumulativeHolds = new ArrayList<>();
+        int running_count = 0;
+        for (Shape shape : panel_set) {
+            cumulativeHolds.add(running_count);
+            running_count += shape.getNumHolds();
+        }
+        return cumulativeHolds;
+    }
+
+    public int findTotalHolds() {
+        /* Find the running total of number of holds.
+
+        Will allow us to locate the holds for a certain panel.
+        */
+        int running_count = 0;
+        for (Shape shape : panel_set) {
+            running_count += shape.getNumHolds();
+        }
+        return running_count;
+    }
 
 }
