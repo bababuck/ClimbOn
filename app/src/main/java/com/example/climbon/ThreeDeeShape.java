eb4f351f9b69839def8d5f775d8db30f00940375
bababuck <buchner.ryan@gmail.com>
2022-03-21 01:09:49 -0700

    Start tpc entralize shapes

---
+++ app/src/main/java/com/example/climbon/ThreeDeeShape.java
@@ -0,0 +1,12 @@
+package com.example.climbon;
+
+import java.nio.FloatBuffer;
+
+public class ThreeDeeShape {
+    protected FloatBuffer vertexBuffer;
+    protected final int mProgram;
+    protected int positionHandle;
+    protected int colorHandle;
+
+    private int vPMatrixHandle;
+}
