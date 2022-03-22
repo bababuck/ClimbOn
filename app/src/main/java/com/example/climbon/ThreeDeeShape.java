eb039505c864661b8d6ab15eb8ddf2c5592b1d74
bababuck <buchner.ryan@gmail.com>
2022-03-22 00:32:43 -0700

    Make single shape for triangles and squares.

--- app/src/main/java/com/example/climbon/ThreeDeeShape.java
+++ app/src/main/java/com/example/climbon/ThreeDeeShape.java
@@ -1,12 +1,81 @@
 package com.example.climbon;
 
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 public class ThreeDeeShape {
     protected FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
     protected final int mProgram;
     protected int positionHandle;
     protected int colorHandle;
 
     private int vPMatrixHandle;
+
+    private final int vertexCount;
+    private final int vertexStride = COORDS_PER_VERTEX * 4;
+
+    static final int COORDS_PER_VERTEX = 3;
+    float coordinates[];
+
+    float color[];
+
+    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+
+    boolean square;
+
+    public ThreeDeeShape(float coordinates[], float color[]) {
+        mProgram = ProgramSingleton.getProgram();
+
+        this.coordinates = coordinates;
+        this.color = color;
+        vertexCount = coordinates.length / COORDS_PER_VERTEX;
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(coordinates.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(coordinates);
+        vertexBuffer.position(0);
+
+        square = (coordinates.length == 12);
+
+        if (square) {
+            // initialize byte buffer for the draw list
+            ByteBuffer dlb = ByteBuffer.allocateDirect(
+                    // (# of coordinate values * 2 bytes per short)
+                    drawOrder.length * 2);
+            dlb.order(ByteOrder.nativeOrder());
+            drawListBuffer = dlb.asShortBuffer();
+            drawListBuffer.put(drawOrder);
+            drawListBuffer.position(0);
+        }
+    }
+
+    public void draw(float[] mvpMatrix) {
+        GLES20.glUseProgram(mProgram);
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+        vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        if (square) {
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 1, vertexCount);
+        }
+        GLES20.glDisableVertexAttribArray(positionHandle);
+    }
 }
