c4ddc1ac24b09eceb9e8810cd72b845369132214
bababuck <buchner.ryan@gmail.com>
2022-03-23 01:22:19 -0700

    Add lines to shapes.

--- app/src/main/java/com/example/climbon/ThreeDeeShape.java
+++ app/src/main/java/com/example/climbon/ThreeDeeShape.java
@@ -10,6 +10,7 @@ import java.nio.ShortBuffer;
 public class ThreeDeeShape {
     protected FloatBuffer vertexBuffer;
     private ShortBuffer drawListBuffer;
+    private ShortBuffer lineListBuffer;
     protected final int mProgram;
     protected int positionHandle;
     protected int colorHandle;
@@ -24,7 +25,11 @@ public class ThreeDeeShape {
 
     float color[];
 
-    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 };
+    private short square_line_order[] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 1};
+    private short triangle_line_order[] = {0, 1, 1, 2, 2, 3, 3, 1};
+
+    float line_color[] = {0.0f, 0.0f, 0.0f, 1.0f};
 
     boolean square;
 
@@ -44,16 +49,22 @@ public class ThreeDeeShape {
 
         square = (coordinates.length == 12);
 
+        ByteBuffer llb = ByteBuffer.allocateDirect(vertexCount * 2);
+        llb.order(ByteOrder.nativeOrder());
+        lineListBuffer = llb.asShortBuffer();
+
         if (square) {
-            // initialize byte buffer for the draw list
-            ByteBuffer dlb = ByteBuffer.allocateDirect(
-                    // (# of coordinate values * 2 bytes per short)
-                    drawOrder.length * 2);
+            ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
             dlb.order(ByteOrder.nativeOrder());
             drawListBuffer = dlb.asShortBuffer();
             drawListBuffer.put(drawOrder);
             drawListBuffer.position(0);
+
+            lineListBuffer.put(square_line_order);
+        } else {
+            lineListBuffer.put(triangle_line_order);
         }
+        lineListBuffer.position(0);
     }
 
     public void draw(float[] mvpMatrix) {
@@ -61,7 +72,6 @@ public class ThreeDeeShape {
         positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         GLES20.glEnableVertexAttribArray(positionHandle);
 
-        // Prepare the triangle coordinate data
         GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT, false,
                 vertexStride, vertexBuffer);
@@ -77,5 +87,27 @@ public class ThreeDeeShape {
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 1, vertexCount);
         }
         GLES20.glDisableVertexAttribArray(positionHandle);
+
+        draw_lines(mvpMatrix);
+    }
+
+    private void draw_lines(float[] mvpMatrix) {
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+
+        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, line_color, 0);
+
+        vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
+
+        for (int i=0;i<(vertexCount+1);++i){
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, i, vertexCount);
+        }
+
     }
 }
