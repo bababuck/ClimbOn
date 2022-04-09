6b98078ff805bddb446f1f83a2e8fd7dba761abe
bababuck <buchner.ryan@gmail.com>
2022-04-09 00:21:45 -0700

    Add save button for route

--- app/src/main/java/com/example/climbon/RouteViewThreeDee.java
+++ app/src/main/java/com/example/climbon/RouteViewThreeDee.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -43,6 +44,9 @@ public class RouteViewThreeDee extends AppCompatActivity {
         String route_description = route_name + ", V" + rating + " " + RouteTypesEnum.values()[route_type];
         text.setText(route_description);
 
+        Button save_button = new Button(this);
+        save_button.setText("Save");
+
         ConstraintLayout layout = new ConstraintLayout(this);
         {
             int GLVIEW_ID = View.generateViewId();
@@ -55,15 +59,26 @@ public class RouteViewThreeDee extends AppCompatActivity {
             layout.addView(text);
         }
         {
+            int SAVE_ID = View.generateViewId();
+            save_button.setId(SAVE_ID);
+            layout.addView(save_button);
+        }
+
+        {
             ConstraintSet constraintSet = new ConstraintSet();
             constraintSet.constrainWidth(text.getId(), constraintSet.MATCH_CONSTRAINT);
             constraintSet.constrainHeight(text.getId(), constraintSet.WRAP_CONTENT);
             constraintSet.constrainWidth(gLView.getId(), constraintSet.MATCH_CONSTRAINT);
             constraintSet.constrainHeight(gLView.getId(), constraintSet.WRAP_CONTENT);
+            constraintSet.constrainWidth(save_button.getId(), constraintSet.MATCH_CONSTRAINT);
+            constraintSet.constrainHeight(save_button.getId(), constraintSet.WRAP_CONTENT);
             constraintSet.connect(text.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,0);
             constraintSet.connect(gLView.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,0);
             constraintSet.connect(text.getId(),ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP,0);
             constraintSet.connect(gLView.getId(),ConstraintSet.TOP,text.getId(),ConstraintSet.BOTTOM,0);
+            constraintSet.connect(gLView.getId(),ConstraintSet.TOP,save_button.getId(),ConstraintSet.BOTTOM,0);
+            constraintSet.connect(save_button.getId(),ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT,0);
+            constraintSet.connect(save_button.getId(),ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP,0);
             constraintSet.applyTo(layout);
         }
         Log.e("ThreeDeeWall","Setting content view...");
